c136bf0f758d64130a9951f297834d907fd4c1e5
shieldcoindevs <shieldcoindevs@hushmail.com>
2014-07-10 04:43:15 -0600

    Cleanup and checkpoints

--- src/net.cpp
+++ src/net.cpp
@@ -1139,6 +1139,8 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
+	{"seed.shieldcoin.net", "seed.shieldcoin.net"},
+	
 };
 
 void ThreadDNSAddressSeed(void* parg)
